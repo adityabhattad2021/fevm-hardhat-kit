ba55d8f702a4475440d1adcc0bc513ac5fc8d2b1
Aditya Bhattad <adityabhattad18@gmail.com>
2023-01-29 18:46:49 +0530

    Filecoin Insurance V0.0.1

--- contracts/InsuranceProvider.sol
+++ contracts/InsuranceProvider.sol
@@ -3,53 +3,124 @@ pragma solidity ^0.8.0;
 
 import {MarketAPI} from "@zondax/filecoin-solidity/contracts/v0.8/MarketAPI.sol";
 import {MarketTypes} from "@zondax/filecoin-solidity/contracts/v0.8/types/MarketTypes.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
+contract FilecoinInsurance {
 
-contract InsuranceProvider {
+    using SafeMath for uint256;
 
-    struct Deal {
-        uint dealId;
-        uint price;
-        bool status;
+    event NewInsuranceRegistered(
+        address indexed insuranceHolder,
+        uint256 indexed dealId,
+        uint256 indexed FILPrice
+    );
+
+    event InsuranceClaimed(
+        address indexed insuranceHolder,
+        uint256 indexed dealId,
+        uint256 indexed FILPrice,
+        uint256 amountPaid
+    );
+
+    struct Deal{
+        string dealLabel;
+        uint64 dealClientActorId;
+        uint64 dealProviderActorId;
+        bool isDealActive;
+        MarketTypes.GetDealClientCollateralReturn dealClientCollateral;
+    }
+    mapping(uint=>Deal) public dealIdToDeal;
+
+    struct Insurance {
+        address insuranceHolder;
+        uint64 dealId;
+        uint64 FILPrice;
+        uint256 collateralLocked;
+        uint256 coverageLimit;
+        uint64 coveragePercentage;
+        uint256 premium;
+        uint256 totalAmountClaimed;
+        uint256 InsuranceExpiry;
+    }
+    mapping(address => Insurance) public holderToInsurance;
+    address[] public insuranceHolders; 
+
+    function getDeal(uint64 dealId) public {
+        string memory dealLabel=MarketAPI.getDealLabel(dealId).label;
+        uint64 dealClientActorId=MarketAPI.getDealClient(dealId).client;
+        uint64 dealProviderActorId=MarketAPI.getDealProvider(dealId).provider;
+        bool isDealActive=MarketAPI.getDealVerified(dealId).verified;
+        MarketTypes.GetDealClientCollateralReturn memory dealClientCollateral=MarketAPI.getDealClientCollateral(dealId);
+        dealIdToDeal[dealId]=Deal({
+            dealLabel:dealLabel,
+            dealClientActorId:dealClientActorId,
+            dealProviderActorId:dealProviderActorId,
+            isDealActive:isDealActive,
+            dealClientCollateral:dealClientCollateral
+        });
     }
 
-    mapping(uint => Deal) public deals;
 
-    function registerStorageDeal(uint dealId) public {
-        // Verify that the deal is active
-        // Get current price of the FIL token while applying for the insurance to store along the deal.
-        // If yes stores the deal in the deals mapping.
-        // If no, throw an error.
-        // TODO: Implement this function.
+// Current Sample Insurance Plan:
+    // 1. coverageLimit=10FIL
+    // 2. coveragePercentage=70%
+    // 3. premium=1FIL
+    function registerInsurance(uint64 dealId,uint64 FILToDollar) payable public {
+        getDeal(dealId);
+        Deal memory newDeal=dealIdToDeal[dealId];
+        require(newDeal.isDealActive,"Deal is not active");
+        // 1000000000000000000=1 FIL
+        require(msg.value<1000000000000000000,"Need to send one 1FIL");
+        // TODO: Check if the msg.sender is the storage provider in the deal.
+        holderToInsurance[msg.sender]=Insurance({
+            insuranceHolder:msg.sender,
+            dealId:dealId,
+            FILPrice:FILToDollar,
+            collateralLocked:50000000000000000000, // 50 FIL (TODO: Get the real value of the collateral locked).
+            coverageLimit:10000000000000000000, // 10 FIL
+            premium:1000000000000000000, // 1 FIL
+            coveragePercentage:70,
+            totalAmountClaimed:0,
+            InsuranceExpiry:block.timestamp+5 minutes
+        });
+        insuranceHolders.push(msg.sender);
+
+        emit NewInsuranceRegistered(
+            msg.sender,
+            dealId,
+            FILToDollar
+        );
     }
 
-    function claimInsurance(uint dealId) public {
-        // Verify the current chainlink price of the FIL token, either on front-end or if possible on the smart contract.
-        // If the price is lower than the price when the deal was registered, then the insurance is paid.
-    }
-
-    function payPremuim(uint dealId) public {
-        // Pay the premium for the insurance.
-    }
-
-    function calcualateLosses(uint dealId) public {
-        // Calculate the losses based on the current price of the FIL token and the price when the deal was registered.
-    }
-
-    function getDeal(uint dealId) public {
-        // Get the deal from the deals mapping.
+    function calculateLosses(uint64 currentFILPrice,uint256 collateralLocked,uint64 oldFILPrice) public pure returns(uint256 totalPayableInFIL){
+        uint256 oldAmount = collateralLocked.mul(oldFILPrice);
+        uint256 newAmount = collateralLocked.mul(currentFILPrice);
+        uint256 losses=oldAmount.sub(newAmount);
+        require(losses>0,"There were no losses");
+        uint256 percentToBePaid=losses.mul(70).div(100);  // 70% of the total losses.
+        totalPayableInFIL=percentToBePaid.div(currentFILPrice);
     }
 
 
-    function getDealPrice(uint dealId) public {
-        // Get the price of the deal from the deals mapping.
-    }
+    function claim(uint dealId,uint64 currentFILPrice) public {
+        Deal memory newDeal=dealIdToDeal[dealId];
+        require(newDeal.isDealActive,"Deal is not active");
+        Insurance storage insuranceData=holderToInsurance[msg.sender];
+        uint64 oldFILPrice=insuranceData.FILPrice;
+        require(currentFILPrice>oldFILPrice,"Current FIL Price < Old FIL Price");
+        uint256 collateralLocked=insuranceData.collateralLocked;
+        uint256 totalPayable=calculateLosses(currentFILPrice,collateralLocked,oldFILPrice);
+        insuranceData.totalAmountClaimed+=totalPayable;
+        (bool success,)=payable(msg.sender).call{value:totalPayable}("");
+        require(success,"Payment failed");
 
-
-    function getDealStatus(uint dealId) public {
-        // Get the status of the deal from the deals mapping.
+        emit InsuranceClaimed(
+            msg.sender,
+            dealId,
+            currentFILPrice,
+            totalPayable
+        );
     }
-    
 
 
 }
